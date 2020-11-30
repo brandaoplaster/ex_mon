595bd691573e6950d836151fe70d59a69879ec71
brandaoplaster <brandaoplaster@gmail.com>
2020-11-30 18:32:17 -0300

    feat: adding status message

---
+++ lib/game/status.ex
@@ -0,0 +1,9 @@
+defmodule ExMon.Game.status do
+  alias ExMon.Game
+
+  def print_round_message() do
+    IO.puts("\n======= The game is started =======\n")
+    IO.inspect(Game.info())
+    IO.puts("---------------------")
+  end
+end
