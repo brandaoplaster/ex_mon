75650a0155ff2132dba2880bbad22efc84daa969
brandaoplaster <brandaoplaster@gmail.com>
2020-12-23 21:18:07 -0300

    feat: adding player attack methods

--- lib/game/actions.ex
+++ lib/game/actions.ex
@@ -1,10 +1,18 @@
 defmodule ExMod.Game.Actions do
-  alias ExMod.Game
+  alias ExMon.Game
+  alias ExMon.Game.Actions.Attack
   
   def fetch_move(move) do
     Game.player()
     |> Map.get(:moves)
     |> find_move(move)
+  end
+
+  def attack(move) do
+    case Game.turn() do
+      :player -> Attack.attack_opponent(:computer, move)
+      :computer -> Attack.attack_opponent(:player, move)
+    end
   end
 
   defp find_move(moves, move) do
