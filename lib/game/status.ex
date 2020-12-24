bae3dcf65c24dad8f31e8ad3f88d2c3433d482d8
brandaoplaster <brandaoplaster@gmail.com>
2020-12-23 21:14:36 -0300

    feat: adding status messages

--- lib/game/status.ex
+++ lib/game/status.ex
@@ -1,9 +1,21 @@
-defmodule ExMon.Game.status do
+defmodule ExMon.Game.Status do
   alias ExMon.Game
 
   def print_round_message() do
     IO.puts("\n======= The game is started =======\n")
     IO.inspect(Game.info())
     IO.puts("---------------------")
+  end
+
+  def print_wrong_move_message(move) do
+    IO.puts("\n======= Invalid move: #{move} =======\n")
+  end
+
+  def print_move_message(:computer, :attack, damage) do
+    IO.puts("\n======= The Player attacked the computer dealing #{damage} damage =======\n")
+  end
+
+  def print_move_message(:player, :attake, damage) do
+    IO.puts("\n======= The computer attacked the Player  dealing #{damage} damage =======\n")
   end
 end
