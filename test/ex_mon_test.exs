95d0855b67a18a16a9ae15381e23f4b93405e40a
brandaoplaster <brandaoplaster@gmail.com>
2021-01-18 18:43:34 -0300

    chore: adding test for create player

--- test/ex_mon_test.exs
+++ test/ex_mon_test.exs
@@ -1,8 +1,18 @@
 defmodule ExMonTest do
   use ExUnit.Case
-  doctest ExMon
+  alias ExMon.{Game, Player}
 
-  test "greets the world" do
-    assert ExMon.hello() == :world
+  describe "create_player/4" do
+    test "returns a player" do
+      expected_response = %Player{
+        name: "Lucas",
+        life: 100, 
+        moves: %{
+          move_avg: :punch, move_heal: :heald, move_rnd: :kick
+        }
+      }
+      
+      assert expected_response == ExMon.create_player("Lucas", :kick, :punch, :heald)
+    end
   end
 end
